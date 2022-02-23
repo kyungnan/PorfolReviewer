2533f074abb73b7631c8f284026dc4572649b17f
kyungnan <baekn1023@gmail.com>
2022-02-23 16:26:24 +0900

    포폴리뷰어 Home 페이지 생성 - 헤더, 푸터 - 배너 - 카테고리 네비게이션 - 베스트 포트폴리오

---
+++ src/main/java/com/reviewer/portfolio/PorfolReviewerApplication.java
@@ -0,0 +1,13 @@
+package com.reviewer.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorfolReviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorfolReviewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reviewer/portfolio/PorfolReviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reviewer.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorfolReviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
