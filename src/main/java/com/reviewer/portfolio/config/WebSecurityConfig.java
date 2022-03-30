78c3b61a0e38e76b3cfa2fc15688965f0a0fbc91
kyungnan <baekn1023@gmail.com>
2022-03-30 13:43:51 +0900

    로그인 유저만 리뷰받기 페이지에 접근 가능하도록 수정

--- src/main/java/com/reviewer/portfolio/config/WebSecurityConfig.java
+++ src/main/java/com/reviewer/portfolio/config/WebSecurityConfig.java
@@ -37,6 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.authorizeRequests()
+			.antMatchers("/porfolUpload").authenticated()
 			.anyRequest().permitAll()
 			.and()
 			.formLogin()
